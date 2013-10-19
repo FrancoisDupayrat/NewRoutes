e5d0059aba9fbbd0514df60509685ca4aefbb15c
François Dupayrat <fradow@gmail.com>
2013-10-19 13:09:51 +0200

    Initial Commit

---
+++ NewRoutes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NewRoutes
+//
+//  Created by François Dupayrat on 19/10/13.
+//  Copyright (c) 2013 Hackathon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NewRoutesTests/NewRoutesTests.m
@@ -0,0 +1,34 @@
+//
+//  NewRoutesTests.m
+//  NewRoutesTests
+//
+//  Created by François Dupayrat on 19/10/13.
+//  Copyright (c) 2013 Hackathon. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NewRoutesTests : XCTestCase
+
+@end
+
+@implementation NewRoutesTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
